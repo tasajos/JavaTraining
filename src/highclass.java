40d9f5d3693f88ac066f1f4557d85b7760623bc6
Carlos.Azcarraga <tasajos@gmail.com>
2022-10-31 18:01:17 -0400

    2.2

---
+++ src/highclass.java
@@ -0,0 +1,17 @@
+public class highclass {
+    public static void main(String[] args) {
+
+        int StudentAge = 17;
+        double Prom = 7.7;
+        char FirstInitial = 'C';
+        char LastInitial= 'S';
+        boolean Attendance = true ;
+
+        System.out.println(StudentAge);
+        System.out.println(Prom);
+        System.out.println(FirstInitial);
+        System.out.println(LastInitial);
+        System.out.println(Attendance);
+
+    }
+}
