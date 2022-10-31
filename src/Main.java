e992a1aab57b764ecf468c88e0bcb9a4eb8c74d4
Carlos.Azcarraga <tasajos@gmail.com>
2022-10-31 15:33:31 -0400

    1.7

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("HelloWorld Nice Trainee");
+        System.out.println("HelloWorld Nice Trainees");
     }
 }
