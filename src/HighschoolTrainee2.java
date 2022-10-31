422177d61f43745e6635c41613dc83ca747942b4
Carlos.Azcarraga <tasajos@gmail.com>
2022-10-31 18:37:54 -0400

    Ejercicio Practico del 2.3 del MR

---
+++ src/HighschoolTrainee2.java
@@ -0,0 +1,25 @@
+public class HighschoolTrainee2 {
+    public static void main(String[] args) {
+
+        int StudentAge = 17;
+        double Prom =7.5;
+        char FirstInitial = 'C';
+        char LastInitial = 'S';
+        boolean Attendance = true ;
+
+        String StudentName= "Carlos Andres";
+        String FirstName="Carlos";
+        String LastName="Azcarraga";
+        System.out.println("The Student Age is "+ StudentAge);
+        System.out.println("The Prom is " + Prom);
+        System.out.println("The First Initial is " + FirstInitial);
+        System.out.println("The last Intial is " + LastInitial);
+        System.out.println("The attendance is "+ Attendance);
+        System.out.println("The student name is "+ StudentName);
+        System.out.println("The student First Name " +FirstName);
+        System.out.println("The student Last Name is "+ LastName);
+
+
+    }
+
+}
