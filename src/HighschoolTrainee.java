da21cbbc52a5763fe35e2ea651ab860fbb586130
Carlos.Azcarraga <tasajos@gmail.com>
2022-10-31 18:09:25 -0400

    Ejercicio Practico del 2.2 del MR

---
+++ src/HighschoolTrainee.java
@@ -0,0 +1,23 @@
+public class HighschoolTrainee {
+    public static void main(String[] args) {
+
+        int StudentAge = 17;
+        double Prom =7.5;
+        char FirstInitial = 'C';
+        char LastInitial = 'S';
+        boolean Attendance = true ;
+
+        System.out.println("The Student Age is "+ StudentAge);
+        System.out.println("The Prom is " + Prom);
+        System.out.println("The First Initial is " + FirstInitial);
+        System.out.println("The last Intial is " + LastInitial);
+        System.out.println("The attendance is "+ Attendance);
+
+
+
+
+    }
+
+}
+
+
