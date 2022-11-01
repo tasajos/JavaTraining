59186f2a156432fc987fbdb1608ee4a429e2e084
Carlos.Azcarraga <tasajos@gmail.com>
2022-11-01 10:02:30 -0400

    Ejercicio Practico del 2.6 del MR

---
+++ src/makingifTrainee.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class makingifTrainee {
+
+    public static void main(String[] args) {
+        int Inputname;
+
+        System.out.println("Pick a number between 1 and 10");
+        Scanner Num = new Scanner(System.in);
+
+        Inputname = Num.nextInt();
+
+        if(Inputname <5){
+            System.out.println("Enjoy the good luck");
+
+        }else {
+            System.out.println("Your show selection will make your very happy");
+        }
+
+    }
+
+}
